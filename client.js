f7e1c8f4d8b2b2c74a0926a15a0ba37abb8a60b1
Francis Begyn <francis@begyn.be>
2025-02-24 16:45:59 +0100

    update socket.io to 4.x
    The original repo is stuck on 2.5.x, but socket.io has moved along. this
    commit brings it up to speed and configures some basic setup for
    multiplex setup. The most notable thing is running the multplex socket
    on `/-/multiplex/socket` path.
    
    Signed-off-by: Francis Begyn <francis@begyn.be>

--- client.js
+++ client.js
@@ -1,22 +1,24 @@
 (function() {
+    var multiplex = Reveal.getConfig().multiplex;
+    var socketId = multiplex.id;
+    var socket = io.connect(multiplex.url, {
+        path: '/-/multiplex/socket',
+        transports: ['websocket'],
+        reconnectionDelayMax: 10000,
+    });
 
-	var multiplex = Reveal.getConfig().multiplex;
-	var socketId = multiplex.id;
-	var socket = io.connect(multiplex.url);
-
-	socket.on(multiplex.id, function(message) {
-		// ignore data from sockets that aren't ours
-		if (message.socketId !== socketId) { return; }
-		if( window.location.host === 'localhost:1947' ) return;
-
-		if ( message.state ) {
-			Reveal.setState(message.state);
-		}
-		if ( message.content ) {
-			// forward custom events to other plugins
-			var event = new CustomEvent('received');
-			event.content = message.content;
-			document.dispatchEvent( event );
-		}
-	});
+    socket.on(multiplex.id, function(message) {
+        // ignore data from sockets that aren't ours
+        if ( message.socketId !== socketId ) { return; }
+        if ( window.location.host === 'localhost:1947' ) return;
+        if ( message.state ) {
+            Reveal.setState(message.state);
+        }
+        if ( message.content ) {
+            // forward custom events to other plugins
+            var event = new CustomEvent('received');
+            event.content = message.content;
+            document.dispatchEvent( event );
+        }
+    });
 }());
--- index.js
+++ index.js
@@ -1,66 +1,70 @@
-let http        = require('http');
-let express		= require('express');
-let cors 		= require('cors')
-let fs			= require('fs');
-let io			= require('socket.io');
-let crypto		= require('crypto');
+let http    = require('http');
+let express = require('express');
+let cors    = require('cors')
+let fs      = require('fs');
+let io      = require('socket.io');
+let crypto  = require('crypto');
 
-let app       	= express();
-let staticDir 	= express.static;
+let app       = express();
+let staticDir = express.static;
 
 app.use(cors()); // enable cors for all origins
 
 let server    	= http.createServer(app);
 
 let socketsIO = io(server, {
-	cors: {
-	  origin: "*",
-	  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS" ]
-	}
-  });
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS" ],
+        transports: ['websocket'],
+    }
+});
 
 let opts = {
-	port: process.env.PORT || 1948,
-	baseDir : process.cwd()
+    port: process.env.PORT || 1948,
+    baseDir : process.cwd()
 };
 
 socketsIO.on( 'connection', socket => {
-	console.debug('Connection opened');
-	socket.on('multiplex-statechanged', data => {
-		if (typeof data.secret == 'undefined' || data.secret == null || data.secret === '') return;
-		if (createHash(data.secret) === data.socketId) {
-			console.debug('Broadcasting state change');
-			data.secret = null;
-			socket.broadcast.emit(data.socketId, data);
-		} else {
-			console.warn('Secret and socketId do not match');
-		}
-	});
+    console.debug('Connection opened');
+    socket.on('multiplex-statechanged', data => {
+        if ( typeof data.secret == 'undefined'
+            || data.secret == null
+            || data.secret === ''
+        ) return;
+        if (createHash(data.secret) === data.socketId) {
+            console.debug('Broadcasting state change');
+            data.secret = null;
+            socket.broadcast.emit(data.socketId, data);
+        } else {
+            console.warn('Secret and socketId do not match');
+        }
+    });
 });
 
 app.use( express.static( opts.baseDir ) );
 
 app.get("/", ( req, res ) => {
-	res.writeHead(200, {'Content-Type': 'text/html'});
+    res.writeHead(200, {'Content-Type': 'text/html'});
 
-	let stream = fs.createReadStream( opts.baseDir + '/index.html' );
-	stream.on('error', error => {
-		res.write('<style>body{font-family: sans-serif;}</style><h2>reveal.js multiplex server.</h2><a href="/token">Generate token</a>');
-		res.end();
-	});
-	stream.on('open', () => {
-		stream.pipe( res );
-	});
+    let stream = fs.createReadStream( opts.baseDir + '/index.html' );
+    stream.on('error', error => {
+        res.write('<style>body{font-family: sans-serif;}</style><h2>reveal.js multiplex server.</h2><a href="/token">Generate token</a>');
+        res.end();
+    });
+    stream.on('open', () => {
+        stream.pipe( res );
+    });
 });
 
 app.get("/token", ( req, res ) => {
-	let secret = crypto.randomBytes(16).toString('hex');
-	res.send({secret: secret, socketId: createHash(secret)});
+    let secret = crypto.randomBytes(16).toString('hex');
+    res.send({secret: secret, socketId: createHash(secret)});
 });
 
 let createHash = (secret) => {
-	let hash = crypto.createHash('sha256').update(secret);
-	return hash.digest('hex');
+    let hash = crypto.createHash('sha256').update(secret);
+    return hash.digest('hex');
 };
 
 // Open the listening port
